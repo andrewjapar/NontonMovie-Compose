e816e0af77dbace02888418515708df51dde2983
andrewjapar <andrewjap.dev@gmail.com>
2021-01-14 23:44:12 +0800

    [UI] Refactor Main Screen

--- app/src/main/java/dev/andrewjap/nontonmovie/MainActivity.kt
+++ app/src/main/java/dev/andrewjap/nontonmovie/MainActivity.kt
@@ -3,19 +3,11 @@ package dev.andrewjap.nontonmovie
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.ui.platform.setContent
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.andrewjap.nontonmovie.presentation.ui.home.HomeViewModel
-import dev.andrewjap.nontonmovie.presentation.ui.home.component.AppDrawer
+import dev.andrewjap.nontonmovie.presentation.ui.home.component.MainScreen
 import dev.andrewjap.nontonmovie.presentation.ui.theme.NontonMovieTheme
-import dev.andrewjap.nontonmovie.presentation.util.BottomNavigationScreens
-import dev.andrewjap.nontonmovie.presentation.util.HomeBottomNavigation
-import dev.andrewjap.nontonmovie.presentation.util.MainScreenNavigationConfigurations
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -28,44 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NontonMovieTheme {
-                val navController = rememberNavController()
-                val scaffoldState = rememberScaffoldState()
-                Scaffold(
-                    scaffoldState = scaffoldState,
-                    drawerContent = {
-                        AppDrawer()
-                    },
-                    bottomBar = {
-                        HomeBottomNavigation(
-                            navController = navController, items = listOf(
-                                BottomNavigationScreens.Home,
-                                BottomNavigationScreens.TV,
-                                BottomNavigationScreens.Movies,
-                                BottomNavigationScreens.Sports
-                            )
-                        )
-                    }
-                ) {
-                    Column {
-                        TopAppBar(
-                            title = { Text(text = "AppBar") },
-                            navigationIcon = {
-                                IconButton(
-                                    onClick = { scaffoldState.drawerState.open() }
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Outlined.Menu
-                                    )
-                                }
-                            }
-                        )
-
-                        MainScreenNavigationConfigurations(
-                            navController = navController,
-                            viewModel = viewModel
-                        )
-                    }
-                }
+                MainScreen(
+                    homeViewModel = viewModel
+                )
             }
         }
     }
---
+++ app/src/main/java/dev/andrewjap/nontonmovie/presentation/ui/home/component/MainScreen.kt
@@ -0,0 +1,61 @@
+package dev.andrewjap.nontonmovie.presentation.ui.home.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.compose.rememberNavController
+import dev.andrewjap.nontonmovie.R
+import dev.andrewjap.nontonmovie.presentation.ui.home.HomeViewModel
+import dev.andrewjap.nontonmovie.presentation.util.BottomNavigationScreens
+import dev.andrewjap.nontonmovie.presentation.util.HomeBottomNavigation
+import dev.andrewjap.nontonmovie.presentation.util.MainScreenNavigationConfigurations
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+/**
+ * Designed and developed by Andrew Japar (@andrewjapar)
+ *
+ */
+
+@ExperimentalCoroutinesApi
+@Composable
+fun MainScreen(
+    homeViewModel: HomeViewModel
+) {
+    val navController = rememberNavController()
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = { AppDrawer() },
+        bottomBar = {
+            HomeBottomNavigation(
+                navController = navController,
+                items = listOf(
+                    BottomNavigationScreens.Home,
+                    BottomNavigationScreens.TV,
+                    BottomNavigationScreens.Movies,
+                    BottomNavigationScreens.Sports
+                )
+            )
+        }
+    ) {
+        Column {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                navigationIcon = {
+                    IconButton(onClick = { scaffoldState.drawerState.open() }) {
+                        Icon(imageVector = Icons.Outlined.Menu)
+                    }
+                }
+            )
+
+            MainScreenNavigationConfigurations(
+                navController = navController,
+                viewModel = homeViewModel
+            )
+        }
+    }
+}
